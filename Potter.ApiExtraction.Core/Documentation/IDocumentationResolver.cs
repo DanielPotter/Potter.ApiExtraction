3424944f5f1bf05c2851205db6281169d9a1b843
Daniel Potter <Daniel@DPotter.net>
2017-11-11 09:17:44 -0800

    Remove the old, overly-complicated implementation

--- Potter.ApiExtraction.Core/Documentation/IDocumentationResolver.cs
+++
@@ -1,9 +0,0 @@
-namespace Potter.ApiExtraction.Core
-{
-    /// <summary>
-    ///     Resolves documentation for a member.
-    /// </summary>
-    public interface IDocumentationResolver
-    {
-    }
-}
--- Potter.ApiExtraction.Core/Documentation/IMemberDocumentation.cs
+++
@@ -1,9 +0,0 @@
-namespace Potter.ApiExtraction.Core
-{
-    /// <summary>
-    ///     provides documentation for a namespace or type member.
-    /// </summary>
-    public interface IMemberDocumentation
-    {
-    }
-}
